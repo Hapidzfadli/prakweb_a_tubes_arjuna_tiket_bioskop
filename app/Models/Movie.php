e555ff9fa3d915de630ce8504229cbe855a730eb
hapidzfadli <hapidzfadli@gmail.com>
2022-12-04 14:44:15 +0700

    Menambahkan fungsi upcoming, schedule dan theater pada Model Movie

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -20,12 +20,43 @@ class Movie extends Model
 
     public static function getDetails($id)
     {
-        $movies = self::getMovie();
-
-        $id = $movies->firstWhere('id', $id);
-        $id = $id['id'];
         $movie = Http::get(self::$urlApi . "movies/$id");
         $movie = $movie->json();
         return collect($movie);
+    }
+
+    public static function getUpcoming()
+    {
+        $upcoming = Http::get(self::$urlApi . "upcoming");
+        $upcoming = $upcoming->json();
+        return collect($upcoming);
+    }
+
+    public static function getCities()
+    {
+        $cities = Http::get(self::$urlApi . "cities");
+        $cities = $cities->json();
+        return collect($cities);
+    }
+
+    public static function getTheaters($city_id)
+    {
+        $theater = Http::get(self::$urlApi . "theaters?city_id=$city_id");
+        $theater = $theater->json();
+        return collect($theater);
+    }
+
+    public static function getSchedules($theater)
+    {
+        $schedules = Http::get(self::$urlApi . "schedules/$theater");
+        $schedules = $schedules->json();
+        return collect($schedules);
+    }
+
+    public static function getSchedulesDetail($theater, $id)
+    {
+        $schedules = Http::get(self::$urlApi . "schedules/$theater/$id");
+        $schedules = $schedules->json();
+        return collect($schedules);
     }
 }
