d31410303eebf433f0355286eaa58b5ee7dd7c57
hasbulahshopari <hasbullahhasbu01@gmail.com>
2022-12-06 15:39:47 +0700

    mengubah table user dan model user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->boolean('is_admin')->nullable();
+            $table->string('no_telphone')->unique()->nullable();
+            $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
