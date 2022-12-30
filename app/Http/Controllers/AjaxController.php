125e8daf78a341eea66bf16c74b7b80953f8bfbe
hapidzfadli <hapidzfadli@gmail.com>
2022-12-30 21:52:01 +0700

    menambahkan fitur search pada halaman orders admin

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dashboard;
 use Illuminate\Http\Request;
 use App\Models\Movie;
 use App\Models\User;
@@ -23,9 +24,19 @@ class AjaxController extends Controller
             return $listmovie;
         } elseif ($request->url == '/dashboard/customers') {
             $customer = User::all()->filter(function ($item) use ($title) {
-                return false !== stristr($item['username'], $title) || false !== stristr($item['name'], $title) || false !== stristr($item['email'], $title) || false !== stristr($item['no_telphone'], $title) || false !== stristr($item['address'], $title);;
+                return false !== stristr($item['username'], $title) || false !== stristr($item['name'], $title) || false !== stristr($item['email'], $title) || false !== stristr($item['no_telphone'], $title) || false !== stristr($item['address'], $title);
             });
             return $customer;
+        } elseif ($request->url == '/dashboard/orders') {
+            $orders = Dashboard::getRecentOrder()->filter(function ($item) use ($title) {
+                if ($item->payment != null) {
+                    return false !== stristr($item->user->name, $title) || false !== stristr($item->user->email, $title) || false !== stristr($item->order_id, $title) || false !== stristr($item->movie, $title) || false !== stristr($item->total_price, $title) || false !== stristr($item->payment->transaction_status, $title);
+                } else {
+                    return false !== stristr($item->user->name, $title) || false !== stristr($item->user->email, $title) || false !== stristr($item->order_id, $title) || false !== stristr($item->movie, $title) || false !== stristr($item->total_price, $title);
+                }
+            });
+
+            return $orders;
         }
     }
 }
